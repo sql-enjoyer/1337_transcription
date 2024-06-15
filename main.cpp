7c9db18f282ae6fd4541c02565a5b1517d7a2125
kirill kirillov <podoksik2008@gmail.com>
2024-06-15 14:12:54 +1000

    string-input2leet, добавил конвертацию в лит

--- main.cpp
+++ main.cpp
@@ -6,13 +6,12 @@
 #include <string>
 using namespace std;
 
-map<char, string> aplphavet {{'a',"4"},{'b',"13"},{'c',"("},{'d',"[)"},
+bool isRunning = true;
+map<char, string> alphavet {{'a',"4"},{'b',"13"},{'c',"("},{'d',"[)"},
 	{'e',"3"},{'f',"|="},{'g',"6"},{'h',"|-|"},{'i',"|"},{'j',"]"},{'k',"|<"},
 	{'l',"1"},{'m',"|Y|"},{'n',"|/|"},{'o',"0"},{'p',"|>"},{'q',"0,"},{'r',"|2"},
 	{'s',"5"},{'t',"7"},{'u',"[_]"},{'v',"√"},{'W',"vv"},{'x',"}{"}, {'y',"`/"},
 	{'z',"2"},{' '," "}};
-bool isRunning = true;
-
 
 void hello_page(){
 	cout << "----------------------------------------------------"<<endl;
@@ -22,17 +21,32 @@ void hello_page(){
 	cout << "----------------------------------------------------"<<endl;			
 }
 
+string input2leet(string input){
+	string output="";
+	for(char ch : input){
+		if(alphavet.count(ch)!=0)
+			output+=alphavet[ch];
+		else 
+			throw 1;
+	}
+	return output;
+}
 
 int main(int argc, char const *argv[])
 {
 	hello_page();
 	string input, output;
 	while(isRunning){
-		output=""; 
 		cout<<"request: ";
 		cin>>input;
+		try{
+			output = input2leet(input);
+		}
+		catch(int code){
+			cout << "error: unknown letter!"<<endl;
+		}
+		cout << output <<endl;;
 
 	}
-
 	return 0;
 }
