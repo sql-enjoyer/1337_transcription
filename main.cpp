8fdc302be9cd98a5097ce844c55b068cf7761b21
kirill kirillov <podoksik2008@gmail.com>
2024-06-15 13:55:23 +1000

    done void-hello_page map-aplphavet

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
+// 1337 transcript to russia
+// 15.06.2024
+
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
+
+map<char, string> aplphavet {{'a',"4"},{'b',"13"},{'c',"("},{'d',"[)"},
+	{'e',"3"},{'f',"|="},{'g',"6"},{'h',"|-|"},{'i',"|"},{'j',"]"},{'k',"|<"},
+	{'l',"1"},{'m',"|Y|"},{'n',"|/|"},{'o',"0"},{'p',"|>"},{'q',"0,"},{'r',"|2"},
+	{'s',"5"},{'t',"7"},{'u',"[_]"},{'v',"√"},{'W',"vv"},{'x',"}{"}, {'y',"`/"},
+	{'z',"2"},{' '," "}};
+bool isRunning = true;
+
+
+void hello_page(){
+	cout << "----------------------------------------------------"<<endl;
+	cout << "            Welcome to the 1337 transcript"<<endl;
+	cout << "                 enter your message"<<endl;
+	cout << "              autor: @812secondsToOff"<<endl;
+	cout << "----------------------------------------------------"<<endl;			
+}
+
 
 int main(int argc, char const *argv[])
 {
-	cout << "Hello World!"<<endl;
+	hello_page();
+	string input, output;
+	while(isRunning){
+		output=""; 
+		cout<<"request: ";
+		cin>>input;
+
+	}
+
 	return 0;
 }
