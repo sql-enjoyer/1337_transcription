68f2ecac734f3dbeb4057c9932303f9ed9a40f0c
kirill kirillov <podoksik2008@gmail.com>
2024-06-20 09:03:42 +1000

    Добавить перевод с лита на английский

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 1337 transcript to russia
+// 1337 translate to russia
 // 15.06.2024
 
 #include <iostream>
@@ -7,8 +7,10 @@
 using namespace std;
 
 bool isRunning = true;
-map<char, string> alphavet {{'a',"4"},{'b',"13"},{'c',"("},{'d',"[)"},
-	{'e',"3"},{'f',"|="},{'g',"6"},{'h',"|-|"},{'i',"|"},{'j',"]"},{'k',"|<"},
+char state = 'l';
+int length;
+map<char, string> alphavet {{'a',"4"},{'b',"|3"},{'c',"("},{'d',"[)"},
+	{'e',"3"},{'f',"|="},{'g',"6"},{'h',"|-|"},{'i',";"},{'j',"]"},{'k',"|<"},
 	{'l',"1"},{'m',"|Y|"},{'n',"|/|"},{'o',"0"},{'p',"|>"},{'q',"0,"},{'r',"|2"},
 	{'s',"5"},{'t',"7"},{'u',"[_]"},{'v',"^"},{'w',"vv"},{'x',"}{"}, {'y',"`/"},
 	{'z',"2"},{' '," "}};
@@ -31,18 +33,8 @@ void help_page(){
 	cout << "             :b                   hello page" << endl;	
 	cout << "             :h                   help page" << endl;
 	cout << "             :q                     quit" << endl;
+	cout << "             ???                     ???" << endl;
 	cout << "----------------------------------------------------"<<endl;
-}
-
-string input2leet(string input){
-	string output="";
-	for(char ch : input){
-		if(alphavet.count(ch)!=0)
-			output+=alphavet[ch];
-		else 
-			throw 1;
-	}
-	return output;
 }
 
 void alphavet_print(){
@@ -75,9 +67,69 @@ void commands(string input){
 	case 'b':
 		hello_page();
 		break;
-	default:
-		cout << "error: unknown command!"<<endl;
+	case 's':{
+		cout << "system: state changed to string"<<endl;
+		state = 's';
+		break;
 	}
+	case 'l':{
+		cout << "system: state changed to leet"<<endl;
+		state = 'l';
+		break; 
+	}
+	case '8':
+		cout<<endl<<"https://www.youtube.com/watch?v=fKxZtiU6NHI"<<endl;
+		break;
+	default:
+		throw 3;
+	}
+}
+
+char find(char ch, int x){
+	for (auto pair : alphavet){
+		if(pair.second[x] == ch) {
+			length = pair.second.length();
+			return pair.first;
+		}
+	}
+	throw 2;
+}
+
+// map<char, string> alphavet {{'a',"4"},{'b',"|3"},{'c',"("},{'d',"[)"},
+// 	{'e',"3"},{'f',"|="},{'g',"6"},{'h',"|-|"},{'i',";"},{'j',"]"},{'k',"|<"},
+// 	{'l',"1"},{'m',"|Y|"},{'n',"|/|"},{'o',"0"},{'p',"|>"},{'q',"0,"},{'r',"|2"},
+// 	{'s',"5"},{'t',"7"},{'u',"[_]"},{'v',"^"},{'w',"vv"},{'x',"}{"}, {'y',"`/"},
+// 	{'z',"2"},{' '," "}};
+
+//[ | 1 0
+string leet2string(string input){
+	string output="";
+	for(int i=0; i<input.size(); ++i){
+		switch(input[i]){
+		case '4': case '(': case '3': case '6': case ';': case ']': case '5': case '1':
+		case '7': case '^': case 'v': case '}': case '`': case '2': case ' ':{
+			output += find(input[i], 0);
+			i += length-1;
+			break;
+			}
+		case '[': case '|': case '0':{
+			output += find(input[i+1], 1);
+			i +=length-1;
+			break;
+			}
+		default: throw 2;
+		}
+	}
+	return output;
+}
+
+string string2leet(string input){
+	string output="";
+	for(char ch : input){
+		if(alphavet.count(ch)!=0) output+=alphavet[ch];
+		else throw 1;
+	}
+	return output;
 }
 
 int main(int argc, char const *argv[])
@@ -85,22 +137,44 @@ int main(int argc, char const *argv[])
 	hello_page();
 	string input, output;
 	while(isRunning){
-		cout<< endl <<"request: ";
-		getline(cin, input);
-
-		if(input[0]==':') {
-			commands(input);
-			continue;
-		}
-
 		try{
-			output = input2leet(input);
-			cout << "leet: " << output << endl;
+			switch(state){
+			case 's': {
+				cout<< endl <<"string: ";
+				getline(cin, input);
+				if(input[0]==':') {
+					commands(input);
+					continue;
+				}
+				output = string2leet(input);
+				cout << "leet: " << output << endl;
+				break;			
+				}
+			case 'l': {
+				cout<< endl <<"leet: ";
+				getline(cin, input);
+				if(input[0]==':') {
+					commands(input);
+					continue;
+				}
+				output = leet2string(input);
+				cout<< "string: "<<output<<endl;
+				break;
+				}
+			}
+		}catch(int code){
+			switch(code){
+			case 1: 
+				cout << "error: unknown letter!"<<endl;
+				break;
+			case 2:
+				cout << "error: unknown symbol!"<<endl;
+				break;
+			case 3:
+				cout << "error: unknown command!"<<endl;
+				break;
+			}
 		}
-		catch(int code){
-			cout << "error: unknown letter!"<<endl;
-		}
-
 	}
 	return 0;
 }
